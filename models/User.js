00de8225f73550d3bd64aaeb5ad8fb35d3e11d28
4ff4 <21117304+4ff4@users.noreply.github.com>
2022-04-06 22:25:40 -0400

    add user model and connect to db

---
+++ models/User.js
@@ -0,0 +1,10 @@
+const { model } = require('mongoose');
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true}
+});
+
+model.exports = mongoose.model('User', UserSchema)
